7fb2c245862c4c256e925a082b35c82ba2513d1a
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 21:30:11 +0530

    trying colors

--- hello_world.sh
+++ hello_world.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 R="\e[31m"
 G= "\e[32m"
+N="\e[0m"
 echo -e "$R helloo $G world"
--- mongodb.sh
+++ mongodb.sh
@@ -3,14 +3,17 @@
 DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G= "\e[32m"
+N="\e[0m"
 
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-    echo " $2 .... failure"
+    echo " $2 .... $R failure"
     exit 1
 else     
-    echo " $2 .... success"
+    echo " $2 .... $G success"
 fi  
 }
 
