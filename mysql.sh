84e673a366cbbae25c80612f1dec7109c0d913a7
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 22:43:38 +0530

    trying colors

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e " $2 .... $R failure $N"
+    exit 1
+else     
+    echo -e " $2 .... $G success$N"
+fi  
+}
+
+USERID=$( id -u )
+if [ $USERID -ne 0 ]
+then 
+   echo "ERROR: run wid root access"
+   exit 1
+fi
+
+yum module disable mysql -y &>>LOGFILE
+VALIDATE $? "disabling mysql" 
+
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>LOGFILE
+VALIDATE $? "copying mysql repo" 
+
+yum install mysql-community-server -y &>>LOGFILE
+VALIDATE $? "installing mysql" 
+
+systemctl enable mysqld &>>LOGFILE
+VALIDATE $? "enabling mysql"
+
+systemctl start mysqld &>>LOGFILE
+VALIDATE $? "enabling mysql"
