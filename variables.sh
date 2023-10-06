76455eef1114768f1338332591ff4978447b12c5
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 13:37:36 +0530

    trying variables

--- variables.sh
+++ variables.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-a=$1
-b=$2 
+
+#1.declaring inside d script
+
+a=100
+b=200
 c=$((a+b))
-DATE=$(date)
 echo "the sum is $c"
+
+#2.run d command inside n store d output in variable
+
+DATE=$(date)
 echo "this is done at $DATE"
+
+#3.can get d values from command line
+name1=$1
+name2=$2
+echo "given names are $name1 and $name2"
+
+#4.get username n password --prompt d user to enter
+echo"enter ur username"
+read USERNAME
+echo"u entered d username as $USERNAME"
+echo"enter ur password"
+read PASSWORD
+echo"u entered d username as $PASSWORD"
+
 
