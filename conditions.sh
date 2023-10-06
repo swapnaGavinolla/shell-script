14c1e4ee1e0818fbfb9c1c43a872e31098289d5d
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 19:41:44 +0530

    trying colors

---
+++ conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USERNAME=$( id -u )
+if [ $USERNAME -ne 0 ]
+then 
+    echo "exit and run with root access"
+    exit 1
+fi
+yum install nginx -y    
+if [ $? -ne 0 ]
+then 
+    echo "installation of nginx is error"
+    exit 1
+else
+    echo "installation of nginx is success"
+fi        
