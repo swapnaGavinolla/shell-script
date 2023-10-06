b28bf9f05780a83574886f3eb46cc3bd3afe8824
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 14:25:27 +0530

    trying variables

--- conditions.sh
+++ conditions.sh
@@ -5,4 +5,11 @@ then
     echo "exit and run with root access"
     exit 1
 fi
-yum install mysql -y     
+yum install mysql -y    
+if [$? ne 0]
+then 
+    echo "installation of mysql is error"
+    exit 1
+else
+    echo "installation of mysql is success"
+fi        
