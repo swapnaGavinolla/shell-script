8d71755aa1394c64103e5f30b23168f888a9e6f4
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 14:37:05 +0530

    trying variables

--- conditions.sh
+++ conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 USERNAME=$(id -u)
-if [$USERNAME -ne 0]
+if [$USERNAME -ne 0 ]
 then 
     echo "exit and run with root access"
     exit 1
 fi
 yum install nginx -y    
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then 
     echo "installation of nginx is error"
     exit 1
