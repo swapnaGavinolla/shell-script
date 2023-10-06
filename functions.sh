8a3e2038c4a707898445fd503a9b1677e3558f22
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 14:55:59 +0530

    trying variables

---
+++ functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VALIDATE(){
+  if[$1 -ne 0]
+  then 
+      echo " $2 installation is .... failure"
+      exit 1
+  else     
+      echo " $2 installation is .... success"
+}
+
+USERID=$(id -u)
+if[$USERID -ne 0]
+then 
+   echo "ERROR: run wid root access"
+   exit 1
+fi
+yum install git -y 
+VALIDATE $? "installing git"
+yum install nginx -y
+VALIDATE $? "installing nginx"
+yum install postfix -y   
+VALIDATE $? "installing postfix"
