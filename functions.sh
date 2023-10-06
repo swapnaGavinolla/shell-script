1e966031db1c142b2567eba031e0a9c462716404
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-06 15:06:35 +0530

    trying variables

--- functions.sh
+++ functions.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+VALIDATE(){
+if [$1 -ne 0]
+then
+    echo " $2 installation is .... failure"
+    exit 1
+else     
+    echo " $2 installation is .... success"
+fi  
+}
+
 USERID=$(id -u)
 if [$USERID -ne 0]
 then 
